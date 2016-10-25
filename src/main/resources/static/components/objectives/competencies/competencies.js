caaa72cb8214140c20c21c4744a2bac12a1139a7
MCINTYRE Chris <chris.mcintyre@soprasteria.com>
2016-10-25 15:38:36 +0100

    Added link to view competency framework
    * Hard coded 8 competencies to be further developed with story 495

---
+++ src/main/resources/static/components/objectives/competencies/competencies.js
@@ -0,0 +1,9 @@
+$(function(){
+    
+    $("#view-competency").click(function(){
+        window.location.href='http://portal.corp.sopra/hr/HR_UK_SG/mycareerpath/LE/Pages/Competency-Framework.aspx';
+    });
+    
+    
+    
+});
