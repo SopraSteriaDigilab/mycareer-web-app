036a037066a8145de95dea881c751e42f3ef61ba
MCINTYRE Chris <chris.mcintyre@soprasteria.com>
2016-11-08 17:48:39 +0000

    getCompetencies from the DB and display
    *css changed to show a selected yellow star
    *method added to check if the star is selected
    *POST method added to update DB of selected competency

--- src/main/resources/static/components/myobjectives/competencies/competencies.js
+++ src/main/resources/static/components/myobjectives/competencies/competencies.js
@@ -15,17 +15,12 @@ $(function() {
 	//Link to competency framework
     $("#view-competency").click(function(){ window.open('http://portal.corp.sopra/hr/HR_UK_SG/mycareerpath/LE/Pages/Competency-Framework.aspx', '_blank'); });
     
-    //change status of competency
-    $(".competency-Details").click(function(){
-        $(this).find('span').toggleClass('glyphicon-star-empty').toggleClass('glyphicon-star');
-    })
-    
-	
+
 });
 
 //Method to make ajax call to add note to database
 function addNoteToDB(userID, from, body ){
-	var url = "http://localhost:8080/addNote/"+userID;
+	var url = "http://127.0.0.1:8080/addNote/"+userID;
 	var data = {};
 	data["from"] = from;
 	data["body"] = body;
@@ -113,7 +108,7 @@ function getCompetencyList(){
         method: 'GET',
         success: function(data){
             $.each(data, function(key, val){
-                addCompetencyToList(val.id,val.title,val.description,val.status);  
+                addCompetencyToList(val.id,val.title,val.compentencyDescription,val.isSelected);  
             });
     },
         error: function(XMLHttpRequest, textStatus, errorThrown){
@@ -123,21 +118,25 @@ function getCompetencyList(){
     });
 }
 
+//Method to add competencies to list display
+function addCompetencyToList(id,title,compentencyDescription,isSelected){
+    $(".competency-Details").append(competenciesListHTML(id,title,compentencyDescription,isSelected));
+}
+
 //Method to return competency html
-function competenciesListHTML(id,title,description,status){
-    
+function competenciesListHTML(id,title,compentencyDescription,isSelected){
     var html = " \
         <div class='panel panel-default'> \
             <div class='panel-heading panel-heading-sm'> \
                 <div class='panel-title'> \
-                    <span class='glyphicon glyphicon-star-empty'></span>" + title + " \
+                <input type='hidden' id='starSelected"+id+"' value='"+isSelected+"'>\
+                    <span class='glyphicon glyphicon-star"+ checkSelected(isSelected) +"' id='star-"+ id +"' onClick='starChanger("+id+")'></span><span id='competencyTitle"+ id +"'>" + title + "</span>  \
                         <a class='collapsed' data-toggle='collapse' href='#collapse-" + id + "'></a> \
                 </div> \
             </div> \
             <div id='collapse-"+id+"' class='panel-collapse collapse'> \
                 <div class='panel-body'> \
-                    <h5><b>"+description+"</b></h5> \
-                    <p></p> \
+                    <h5>"+compentencyDescription+"</h5> \
                 </div> \
             </div> \
         </div><!--End of panel --> \
@@ -145,12 +144,61 @@ function competenciesListHTML(id,title,description,status){
     return html;
 }
 
-//Method to add competencies to list display
-function addCompetencyToList(id,title,description,status){
-    $(".competency-Details").append(competenciesListHTML(id,title,description,status));
+//Method to change star icon to selected or not
+function checkSelected(isSelected){
+    if(isSelected){
+        return "";
+    }else{
+        return "-empty";
+    }
 }
 
-//Method to make Ajax call and return clicked competencies to DB
+//send request to backend to update star to be selected or de-selected
+function starChanger(id){
+    var status = ($('#starSelected'+id).val() === "true") ? false : true;
+    var title = $('#competencyTitle'+id).text();
+    var userID = 2312;
+    
+    updateCompetencyStatus(userID,id,title,status);
 
+}
+
+function test(bool){
+    if(!bool){
+        return true;
+    }else{
+        return false;
+    }
+}
+//Method to make Ajax call and return clicked competencies to DB
+function updateCompetencyStatus(userID, id, title, status){
+    var url = "http://127.0.0.1:8080/updateCompetency/2312";
+	var data = {};
+	data["title"] = title;
+	data["status"] = status;
+    
+	var settings = {
+	  "url": url,
+	  "method": "POST",
+	  "data": data
+	}
+
+	$.ajax(settings).done(function (response) {
+	  toastr.success(response);
+        
+    var className = $('#star-'+id).attr('class');
+    if (className.indexOf("empty")>=0){
+        $('#star-'+id).removeClass("glyphicon-star-empty");
+        $('#star-'+id).addClass("glyphicon-star");
+        $('#starSelected'+id).val("true");
+    }else{
+        $('#star-'+id).removeClass("glyphicon-star");
+        $('#star-'+id).addClass("glyphicon-star-empty");
+        $('#starSelected'+id).val("false");
+    }
+	});
+    
+    
+}
 
     
