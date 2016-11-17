a8b85120f4a2f60ceba4ab1fd59a53c921d829ea
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2016-11-17 18:10:29 +0000

    Implemented status bar.
    * Status bar now works and sets objective to the correct tab.

--- src/main/resources/static/components/myobjectives/myobjectives.js
+++ src/main/resources/static/components/myobjectives/myobjectives.js
@@ -18,13 +18,6 @@ $(function() {
 	//onClick for Submit modal
 	$('#submit-obj').click(function(){ clickSubmitObjective(); });
 
-
-	//3 onclicks to change the progress of the status bar of an objective
-	//status goes from Awaiting, InFlight, Done
-	$('.one').click(function(){ updateProgressBar(0); });
-	$(".two").click(function(){ updateProgressBar(50); });
-	$(".three").click(function(){ updateProgressBar(100); });
-
     //Navigation Pills to show All/Awaiting/InFlight/Done objectives
     $("#navTab").click(function(){});
 
@@ -42,6 +35,7 @@ function getObjectivesList(){
           	nextObjID = val.id;
           	var expectedBy = formatDate(val.timeToCompleteBy);
           	addObjectiveToList(val.id, val.title, val.description, expectedBy, val.progress, val.isArchived);
+//          	updateStatusOnList(val.id, val.progress);
           });
       },
       error: function(XMLHttpRequest, textStatus, errorThrown){
@@ -108,7 +102,6 @@ function openEditObjectiveModal(id){
 	var objText = $('#obj-text-'+id).text().trim();
 	var objDate = $('#obj-date-'+id).text().trim();
 	var objStatus = $('#obj-status-'+id).val();
-//	var objIsArchived = $('#obj-is-archived-'+id).val();
 	objDate = reverseDateFormat(objDate);
 	setObjectiveModalContent(objID, objTitle, objText, objDate, objStatus, false);
 	showObjectiveModal(true);
@@ -124,7 +117,7 @@ function clickSubmitObjective(){
 	var objTitle = $("#objective-title").val().trim();
 	var objText = $("#objective-text").val().trim();
 	var objDate = $("#objective-date").val().trim();
-	alert(objDate);
+//	alert(objDate);
 	var objStatus = parseInt($("#objective-status").val());
 	var objIsArchived = $("#objective-is-archived").val()
 
@@ -142,10 +135,10 @@ function clickSubmitObjective(){
 //Function to add objective to list
 function addObjectiveToList(id, title, description, expectedBy, status, isArchived){
 	var listID = "";
-		if(isArchived){
+		if(isArchived === true || isArchived === 'true'){
 			listID = "obj-archived";
 		}else{
-		switch(status){
+		switch(parseInt(status)){
 			case 0: 
 				listID = statusListDivIDs[status];
 				break;
@@ -192,6 +185,8 @@ function showObjectiveModal(show){
 //Method to handle the archive objective button
 function clickArchiveObjective(objID){
 	var archive = !!$("#obj-is-archived-"+objID).val();
+	$('#obj-is-archived-'+objID).val(archive);
+//	alert(archive);
 	editObjectiveArchiveOnDB(objID, archive);
 	updateObjectiveList(objID);
 }
@@ -214,15 +209,15 @@ function editObjectiveArchiveOnDB(objID, archive){
 }
 
 function updateObjectiveList(objID){
-//	alert(objID);
 	var title = $('#obj-title-'+objID).text();
 	var description = $('#obj-text-'+objID).text();
 	var expectedBy = $('#obj-date-'+objID).text();
 	var status = $('#obj-status-'+objID).val();
-	var archive = !!$('#obj-is-archived-'+objID).val();
-	
+	var archive = $('#obj-is-archived-'+objID).val();
+//	alert(title + " : " + status + " : " + archive);
 	$("#objective-item-"+objID).remove();
 	addObjectiveToList(objID, title, description, expectedBy, status, archive);
+	
 	
 }
 
@@ -233,6 +228,42 @@ function clickObjectiveFeedback(id){
 	window.location.replace("http://localhost:8000/mycareer/feedback"); 
 }
 
+function updateStatusOnDB(objID, objStatus){
+	var userID = getADLoginID();
+	var objTitle = $('#obj-title-'+objID).text();
+	var objText = $('#obj-text-'+objID).text();
+	var objDate = $('#obj-date-'+objID).text();
+	objDate = reverseDateFormat(objDate);
+	$('#obj-status-'+objID).val(parseInt(objStatus));
+	
+	editObjectiveOnDB(userID, objID, objTitle, objText, objDate, objStatus);
+//	updateStatusOnList(objID, objStatus);
+	updateObjectiveList(objID);
+}
+//
+//function updateStatusOnList(objID, objStatus){
+//	switch(parseInt(objStatus)){
+//		case 0:
+//			$('#started-obj-dot-'+objID).removeClass('complete');
+//			$('#complete-obj-dot-'+objID).removeClass('complete');
+//			break;
+//		case 1:
+//			$('#started-obj-dot-'+objID).addClass('complete');
+//			$('#complete-obj-dot-'+objID).removeClass('complete');
+//			break;
+//		case 2:
+//			$('#started-obj-dot-'+objID).addClass('complete');
+//			$('#complete-obj-dot-'+objID).addClass('complete');
+//	}
+//	
+//	
+//}
+
+function checkComplete(status, item){
+	if(status >= item){
+		return 'complete';
+	}
+}
 
 //Function that returns objective list in html format with the parameters given
 function objectiveListHTML(id, title, description, timeToCompleteBy, status, isArchived){
@@ -253,17 +284,17 @@ function objectiveListHTML(id, title, description, timeToCompleteBy, status, isA
 	            		</div> \
             		</div> \
             		<div class='col-sm-5 bs-wizard'> \
-            			 <div class='col-xs-4 bs-wizard-step complete'> \
-					      <div class='text-center '><h6>Proposed</h6></div> \
+            			 <div class='col-xs-4 bs-wizard-step complete' id='proposed-obj-dot-"+id+"' onClick='updateStatusOnDB("+id+", 0)'> \
+					      <div class='text-center' id='test'><h6>Proposed</h6></div> \
 					      <div  class='bs-wizard-dot-start' style='cursor:pointer'></div> \
 					     </div> \
-					     <div class='col-xs-4 bs-wizard-step '> \
+					     <div class='col-xs-4 bs-wizard-step "+ checkComplete(status, 1) +"' id='started-obj-dot-"+id+"' onClick='updateStatusOnDB("+id+", 1)'> \
 					       <div class='text-center'><h6>Started</h6></div> \
 					       <div class='progress'><div class='progress-bar'></div></div> \
 					       <div  class='bs-wizard-dot-start' style='cursor:pointer'></div> \
 					       <div  class='bs-wizard-dot-complete' style='cursor:pointer'></div> \
 					     </div> \
-					     <div class='col-xs-4 bs-wizard-step '> \
+					     <div class='col-xs-4 bs-wizard-step  "+ checkComplete(status, 2) +"' id='complete-obj-dot-"+id+"' onClick='updateStatusOnDB("+id+", 2)'> \
 					       <div class='text-center'><h6>Completed</h6></div> \
 					       	 <div class='progress'><div class='progress-bar'></div></div> \
 					        <div class='bs-wizard-dot-start' style='cursor:pointer'></div> \
