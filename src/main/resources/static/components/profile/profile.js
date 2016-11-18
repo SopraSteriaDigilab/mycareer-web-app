d7843bda71142428d698a4dfe553d37f9b4c22b8
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2016-11-18 17:08:28 +0000

    Started work on my team
    * Created my team page.
    * Shows up list of reportees.
    * placeholders for reportees career and feed.

--- src/main/resources/static/components/profile/profile.js
+++ src/main/resources/static/components/profile/profile.js
@@ -7,13 +7,7 @@ $(function() {
 
 function populateProfile(userName, ADUserName){
 	$("#userProfileName").append("<h4 class='profile-centre'>" + ADUserName + "</h4>");
-	$("#userProfilePicture").append("<img class='backup_picture' src='http://mysite.corp.sopra/User%20Photos/Images%20du%20profil/"
-									+ userName + "_SThumb.jpg?t=1479114656424' alt='' onerror='imgError(this);'> ");
+	$("#userProfilePicture").append(getProfilePicture(ADUserName, 48));
 }
 
-function imgError(image){
-		image.onerror=false;
-    	image.src = "http://teams.duns.uk.sopra/_layouts/15/images/PersonPlaceholder.42x42x32.png?";
-    	image.style = "min-width:48px; min-height:48px; clip:rect(0px, 48px, 48px, 0px); max-width:48px";
-    	return true;
-}
+
