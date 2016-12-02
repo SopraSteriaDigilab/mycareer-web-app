6f3f551ad5185d2fe1f10f7ec1e3a7f6592e3139
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2016-12-02 17:15:40 +0000

    Added templates for link to note type.
    * Waiting on back end for the request.

--- src/main/resources/static/components/notes/notes.js
+++ src/main/resources/static/components/notes/notes.js
@@ -4,8 +4,6 @@ $(function() {
 	
 	//Get general-notes
 	getNotesList(getADLoginID());
-	
-	
     	
 	$("#notes-open").click(function(e) { openNotesBar() });
 	 
@@ -18,22 +16,22 @@ $(function() {
 	$('#submit-note').click(function(){ clickSubmitNote(); });
 	
 	
-
-	
 });
+
+var noteTypeList = ["General", "Objectives", "Competencies", "Feedback", "Development", "Team"];
 
 function initResizable(){
 	$( "#resizable" ).resizable({
-		 minWidth: 200,
-		 handles: "e, w"
+		 minWidth: 300,
+		 handles: "w"
 	});
-	$('.selectpicker').selectpicker({});
 	
-	$('.selectpicker').on('change', function(){
+	$('.selectpicker').selectpicker({ width: 'fit'});
+	
+	$('#note-type-picker').on('change', function(){
 	    var selected = $(this).find("option:selected").val();
 	    showSection(selected);
 	  });
-
 }
 
 
@@ -46,7 +44,7 @@ function getNotesList(userID){
           $.each(data, function(key, val){
           	
           	var date = timeStampToDateTime(new Date(val.timeStamp));
-          	addNoteToList(val.fromWho, val.body, date);
+          	addNoteToList(val.fromWho, val.noteType, val.linkID, val.body, date);
           });
       },
       error: function(XMLHttpRequest, textStatus, errorThrown){
@@ -57,18 +55,42 @@ function getNotesList(userID){
 }
 
 //Method to add note to list directly
-function addNoteToList(fromWho, body, date){
-	$("#general-notes-list").prepend(notesListHTML(fromWho, body, date));
+function addNoteToList(fromWho, noteType, linkID, body, date){
+	var listID = "";
+	switch(parseInt(noteType)){
+		case 0:
+			listID = "general";
+			break;
+		case 1:
+			listID = "objective";
+			break;
+		case 2:
+			listID = "competency";
+			break;
+		case 3:
+			listID = "feedback";
+			break;
+		case 4:
+			listID = "development";
+			break;
+		case 5:
+			listID = "team";
+			break;
+	}
+	$("#"+listID+"-notes-list").prepend(notesListHTML(fromWho, noteType, linkID, body, date));
 }
 
 //Method to return html
-function notesListHTML(fromWho, body, date){
-
+function notesListHTML(fromWho, noteType, linkID, body, date){
+	var link="";
+	if(linkID>0) link = "#" + linkID; 
+	
 	var html = " \
 	  <li class='list-group-item'> \
 	  	<div class='row'> \
 			<div class='col-md-6 wrap-text'><h6 ><b>" + fromWho + "</b></h6></div> \
-			<div class='col-md-6'><h6 class='pull-right'><b>" + date + "</b></h6></div> \
+			<div class='col-md-2'><h6 class='pull-right'><b> " + link + "</b></h6></div> \
+			<div class='col-md-4'><h6 class='pull-right'><b>" + date + "</b></h6></div> \
 		</div> \
 		<div class='row'> \
 			<div class='col-md-12 wrap-text'><p>" + body + "</p></div> \
@@ -78,16 +100,24 @@ function notesListHTML(fromWho, body, date){
 	return html;	
 }
 
+function addLinkID(id){
+	if(id > 0){
+		return id;
+	}
+}
+
 
 //Method to get data and submit data
 function clickSubmitNote(){
 	var userID = getADLoginID();
+	var noteTypeText = $("#note-type-picker").find("option:selected").val();
+	var noteType = noteTypeList.indexOf(noteTypeText);
+	var linkID = 0;
 	var note = $('#note-text').val().trim();
 	var from = getADfullName();
 	var date = timeStampToDateTime(new Date());
-	
-	addNoteToDB(userID, from, note);
-	addNoteToList(from, note, date);
+	addNoteToDB(userID, noteType, linkID, from, note);
+	addNoteToList(from, noteType, linkID, note, date);
 	
 	$('#note-text').val('');
 	$('#submit-note').prop("disabled", true);
@@ -95,9 +125,11 @@ function clickSubmitNote(){
 
 
 //Method to make ajax call to add note to database
-function addNoteToDB(userID, from, body ){
+function addNoteToDB(userID, noteType, linkID, from, body ){
 	var url = "http://127.0.0.1:8080/addNote/"+userID;
 	var data = {};
+	data["noteType"] = noteType;
+	data["linkID"] = linkID;
 	data["from"] = from;
 	data["body"] = body;
     
@@ -118,7 +150,7 @@ function showSection(section){
 		if($(this).hasClass(section)){
 			$(this).fadeIn(600);
 		}
-	})
+	});
 	
 	
 }
