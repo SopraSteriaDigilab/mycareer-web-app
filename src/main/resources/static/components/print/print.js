41e2638a7b0a1bf0409cc007191ea0502c6d6d87
abrard <alexandre.brard@soprasteria.com>
2017-04-03 09:57:18 +0100

    little changes

--- src/main/resources/static/components/print/print.js
+++ src/main/resources/static/components/print/print.js
@@ -195,7 +195,7 @@ function printObjectivesList(dueDate, title, description, progress, createdOn, p
 	var html = " \
             <tr> \
                 <td>"+dueDate+"</td> \
-                <td><span style=\"font-weight: bold;\">"+title+"</span><br/>"+description+"</td> \
+                <td><div class=\"wrap-text\"><span style=\"font-weight: bold;\">"+title+"</span><br/>"+description+"</div></td> \
                 <td>"+progress+"</td> \
                 <td>"+timeStampToLongDate(createdOn)+"</td> \
                 <td>"+proposedBy+"</td> \
