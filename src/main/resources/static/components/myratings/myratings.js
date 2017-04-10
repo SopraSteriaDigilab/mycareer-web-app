4b4462005c95e817e4fbc9294b76435dfda0123f
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2017-04-10 15:24:10 +0100

    finished myratings page withouth ajax calls

--- src/main/resources/static/components/myratings/myratings.js
+++ src/main/resources/static/components/myratings/myratings.js
@@ -2,45 +2,89 @@ $(function() {
 	init();
 });
 
-//DOM element refrences
-var $edit = $("#edit-self-evaluation");
-var $save = $("#save-self-evaluation");
+/** Constants */
+const NO_SELF_EVALUATION = "No self evaluation has been written.";
+const NO_MANAGER_EVALUATION = "No manager evaluation has been written.";
+const NO_RATING = "No Rating Entered";
+const RATING = "Rating: ";
+
+/** DOM element references */
+var $editButtons = $(".edit-buttons");
+var $saveCancelButtons = $(".save-cancel-buttons");
 var $selfEvaluationText = $("#self-evaluation-text");
 var $selfEvaluationInput = $("#self-evaluation-input");
 var $managerEvaluationText = $("#manager-evaluation-text");
-var $managerEvaluationScore = $("#manager-evaluation-score");
+var $evaluationScore = $("#evaluation-score");
 
-//Constants
-var NO_SELF_EVALUATION = "No self evaluation has been written.";
-var NO_MANAGER_EVALUATION = "No manager evaluation has been written.";
-var NO_RATING = "No Rating Entered";
+/** Variables */
+var tempSelfEvaluation = "";
 
-//Initialise MyRatings Page.
+/** Initialise MyRatings Page. */
 function init(){
 	getMyRatings();
 	
-	$("#edit-self-evaluation").click(function(){ clickEditSelfEvaluation(); });
-	$("#save-self-evaluation").click(function(){ clickSaveSelfEvaluation(); });
+	$("#edit-self-evaluation").click(function(){ editSelfEvaluation(); });
+	$("#save-self-evaluation").click(function(){ saveSelfEvaluation(); });
+	$("#cancel-self-evaluation").click(function(){ closeSelfEvaluation(false); });
 }
 
+/** Sets the three evaluations in the HTML */
+function setMyRatings(selfEvaluation, managerEvaluation, evaluationScore){
+	var manager = (managerEvaluation == "") ? NO_MANAGER_EVALUATION : managerEvaluation;
+	var score = (evaluationScore == 0) ? NO_RATING : RATING + evaluationScore;
+	
+	setSelfEvaluationLabel(selfEvaluation);
+	$selfEvaluationInput.val(selfEvaluation);
+	$managerEvaluationText.text(manager);
+	$evaluationScore.text(score);
+}
+
+/** Sets the self evaluation label */
+function setSelfEvaluationLabel(selfEvaluation){
+	var self = (selfEvaluation == "") ? NO_SELF_EVALUATION : selfEvaluation;
+	$selfEvaluationText.text(self);
+}
+
+/** Retrieve MyRatings details from database and update relevant DOM Elements. */
 function getMyRatings(){
 	console.log("getting my ratings");
-	$selfEvaluationText.text(NO_SELF_EVALUATION);
-	$managerEvaluationText.text(NO_MANAGER_EVALUATION);
-	$managerEvaluationScore.text(NO_RATING);
+	const data = {
+		"selfEvaluation":"",
+		"managerEvaluation":"",
+		"evaluationScore": 0 
+	};
+	//TODO Ajax Request to get ratings.
+	setMyRatings(data.selfEvaluation, data.managerEvaluation, data.evaluationScore); //In success function
 }
 
-function clickEditSelfEvaluation(){
-	$edit.hide();
+/** Save self evaluation to the database. */
+function saveSelfEvaluation(){
+	console.log("saving my self evaluation: " + $selfEvaluationInput.val());
+	//TODO Ajax request to save self evaluation.
+	closeSelfEvaluation(true); //In Success function 
+}
+
+/** Make self evaluation editable. */
+function editSelfEvaluation(){
+	$editButtons.hide();
 	$selfEvaluationText.hide();
-	$save.show();
-	$selfEvaluationInput.text($selfEvaluationText.text().trim()).show();
+	$saveCancelButtons.show();
+	$selfEvaluationInput.show();
+	tempEvaluation = $selfEvaluationInput.val();
 }
 
-function clickSaveSelfEvaluation(){
-	$save.hide();
+/**
+ * Hide editable self evaluations
+ * @param save true to save, false to cancel
+ */
+function closeSelfEvaluation(save){
+	$saveCancelButtons.hide();
 	$selfEvaluationInput.hide();
-	$edit.show();
-	$selfEvaluationText.text($selfEvaluationInput.val()).show();
+	$editButtons.show();
+	if(save) {
+		setSelfEvaluationLabel($selfEvaluationInput.val());
+	}else{
+		$selfEvaluationInput.val(tempSelfEvaluation);
+	}
+	$selfEvaluationText.show()
 }
-
