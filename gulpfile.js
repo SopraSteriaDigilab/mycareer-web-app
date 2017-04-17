1615dbebe3f9f89658fa6771e674e9e91975acee
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2017-04-17 15:53:13 +0100

    testing gulp minify js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('minify', function(cb) {
+  var files = 'src/main/resources/static/components/**/*.js'; // matches multiple files
+  pump([
+  		gulp.src(files),
+  		uglify(),
+  		rename({ suffix: '.min' }),
+  		gulp.dest('./src/main/resources/static/dist')
+  	],
+  	cb
+	);
+});
