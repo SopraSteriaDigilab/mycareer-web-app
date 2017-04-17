cd8ac5ad4f8f72f0f38deb0b4c109750e30ca721
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2017-04-17 13:25:06 +0100

    moved actions folder

--- src/main/resources/static/actions/ratingactions.js
+++ src/main/resources/static/actions/ratingactions.js
@@ -1,4 +1,10 @@
-function getCurrentRatingAction(userId, successFunction){
+/**
+ * Ajax GET call to get current ratings for a user.
+ * 
+ * @param userId the id of the user
+ * @param successFuntion function if call succeeds
+ */
+function getCurrentRatingAction(userId, successFunction, errorFunction){
     $.ajax({
         url: 'http://'+getEnvironment()+'/getCurrentRating/'+userId,
         cache: false,
@@ -10,6 +16,7 @@ function getCurrentRatingAction(userId, successFunction){
         error: function(XMLHttpRequest, textStatus, errorThrown){        	
             console.log('error', errorThrown);
             toastr.error("Sorry, there was a problem getting ratings, please try again later.");
+            errorFunction();
         }
     });
 }
