21951bd60a78e7cfd5f899339b3213425cec55f4
abrard <alexandre.brard@soprasteria.com>
2017-04-24 16:42:17 +0100

    email modal : fixed issue with internet explorer (used array instead of set)

--- src/main/resources/static/components/profile/profile.js
+++ src/main/resources/static/components/profile/profile.js
@@ -130,7 +130,7 @@ function getEmails(){
 /** Sets the email addresses in the HTML */
 function setEmailAddresses(sopraSteriaEmailAddresses,preferedEmailAddress){
 	
-	var sopraSteriaEmailsHTML = '<ul><li class="ui-menu-item" role="menuitem">' + Array.from(sopraSteriaEmailAddresses).join('</li><li>') + '</li></ul>';
+	var sopraSteriaEmailsHTML = '<ul><li class="ui-menu-item" role="menuitem">' + sopraSteriaEmailAddresses.join('</li><li>') + '</li></ul>';
 	var extraEmail = (preferedEmailAddress == null || preferedEmailAddress == '') ? NO_EXTRA_EMAIL_ADDRESS : preferedEmailAddress;
 
 	$addEmailInput.val(preferedEmailAddress);
