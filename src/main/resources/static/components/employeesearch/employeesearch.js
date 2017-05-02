6a34102067993e8b1117105325ce272debb83803
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2017-05-02 16:52:02 +0100

    added objectives datatables to employee search.

--- src/main/resources/static/components/employeesearch/employeesearch.js
+++ src/main/resources/static/components/employeesearch/employeesearch.js
@@ -2,29 +2,70 @@ $(function() {
 	init();
 });
 
-$employeeSelectPicker = $('#employee-search-seletpicker');
+var $employeeSelectPicker = $('#employee-search-seletpicker');
+var $tabs = $(".tab-pane");
+
+var $objectivesTable = ("#objectives-table");
+
+var table = "";
+
+var retrievedEmployees = [];
+
+var objectivesColumnList = [ { data: "id" }, { data: "title" }, { data: "description" }, { data: "progress" }, { data: "dueDate" }, { data: "createdOnAsDate" }, { data: "proposedBy" }, { data: "archived" }]
+
 
 function init(){
 	
-	getEmployeeCareer();
-	
-	//init selectpicker
+	//initialise selectpicker
 	$employeeSelectPicker.selectpicker();
 
 	//listeners
 	$employeeSelectPicker.on('change', function() { selectEmployee($(this).val()) });
+	
 }
-
 
 function selectEmployee(employeeId){
-	alert(employeeId);
+	if(!employeeRetrieved(employeeId)){
+		getEmployeeCareer(employeeId);
+	}else{
+		updateEmployeeView(employeeId, retrievedEmployees[employeeId]);
+	}
 }
 
-function getEmployeeCareer(){
-	var employeeId = 675590;
-	getEmployeeCareerAction(675590, function(data){populateHTML(data)}, function(error){});
+function getEmployeeCareer(employeeId){
+	getEmployeeCareerAction(employeeId, function(data){
+		addEmployee(employeeId, data)
+	}, function(error){});
 }
 
-function populateHTML(){
-	
+function addEmployee(employeeId, data){
+	retrievedEmployees[employeeId] = data;
+	updateEmployeeView(employeeId, data);
+}
+
+function getTable(selectorId, dataset, columnsList){
+
+	if ($.fn.dataTable.isDataTable(selectorId) ) {
+	    table = $(selectorId).dataTable();
+	    table.fnClearTable();
+	    table.fnAddData(dataset);
+	}else{
+	 table = $(selectorId).dataTable({
+        data: dataset,
+        columns: columnsList
+	 });
+	}
+}
+
+function updateEmployeeView(employeeId, data){
+	getTable($objectivesTable, data.objectives, objectivesColumnList);
+}
+
+function employeeRetrieved(employeeId){
+	if(jQuery.inArray(employeeId, Object.keys(retrievedEmployees)) == -1){
+		console.log("adding : " + employeeId);
+		return false;
+	}
+	console.log("already exists : " + employeeId);
+	return true;
 }
