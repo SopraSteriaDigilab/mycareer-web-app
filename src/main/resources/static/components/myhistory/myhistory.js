e1b4814b81f2150d460a6c84940ed12430a85576
abrard <alexandre.brard@soprasteria.com>
2017-05-04 10:16:16 +0100

    History section : changed link color, removed unecessary code, removed ID column and formated dates

--- src/main/resources/static/components/myhistory/myhistory.js
+++ src/main/resources/static/components/myhistory/myhistory.js
@@ -8,11 +8,17 @@ var $historyDevelopmentNeedsTable = ("#history-development-needs-table");
 var $historyNotesTable = ("#history-notes-table");
 var $historyRatingsTable = ("#history-ratings-table");
 
-var objectivesColumnList = [ { data: "id" }, { data: "title" }, { data: "description" }, { data: "progress" }, { data: "dueDate" }, { data: "createdOnAsDate" }, { data: "proposedBy" }, { data: "archived" }];
-var feedbackColumnList = [ { data: "id" }, { data: "providerEmail" }, { data: "feedbackDescription" }, {data: "timestamp"}];
-var developmentNeedsColumnList = [ { data: "id" }, { data: "title" }, { data: "description" }, { data: "progress" }, { data: "dueDate" }, { data: "createdOnAsDate" }, { data: "proposedBy" }, {data: "category"}, { data: "archived" }];
-var notesColumnList = [ { data: "id" }, { data: "providerName" }, { data: "noteDescription" }, {data: "timestamp"} ];
+var objectivesColumnList = [ { data: "title" }, { data: "description" }, { data: "progress" }, { data: "dueDate" }, { data: "createdOnAsDate" }, { data: "proposedBy" }, { data: "archived" }];
+var feedbackColumnList = [ { data: "providerEmail" }, { data: "feedbackDescription" }, {data: "timestamp"}];
+var developmentNeedsColumnList = [ { data: "title" }, { data: "description" }, { data: "progress" }, { data: "dueDate" }, { data: "createdOnAsDate" }, { data: "proposedBy" }, {data: "category"}, { data: "archived" }];
+var notesColumnList = [ { data: "providerName" }, { data: "noteDescription" }, {data: "timestamp"} ];
 var ratingsColumnList = [ { data: "year" }, { data: "selfEvaluation" }, { data: "managerEvaluation" }, { data: "score" }];
+
+var objectivesColumnDefs = [{ width: "25%", targets: [0,1] }, {render: function(data, type, row){ return formatDate(data)}, targets:3}, {render: function(data, type, row){ return timeStampToLongDate(data)}, targets:4}];
+var feedbackColumnDefs = [{ width: "60%", targets: 1 }, {render: function(data, type, row){ return timeStampToDateTime(data)}, targets:2}];
+var developmentNeedsColumnDefs = [{ width: "25%", targets: [0,1] }, {render: function(data, type, row){ return formatDate(data)}, targets:3}, {render: function(data, type, row){ return timeStampToLongDate(data)}, targets:4}];
+var notesColumnDefs = [{ width: "60%", targets: 1 }, {render: function(data, type, row){ return timeStampToDateTime(data)}, targets:2}];
+var ratingsColumnDefs = [{ width: "30%", targets: [1,2] }];
 
 function init() {
     $('.selectpicker').selectpicker();
@@ -27,22 +33,17 @@ function getMyCareer(){
 }
 
 function getTables(data){
-	getTable($historyObjectivesTable, data.objectives, objectivesColumnList);
-	getTable($historyFeedbackTable, data.feedback, feedbackColumnList);
-	getTable($historyDevelopmentNeedsTable, data.developmentNeeds, developmentNeedsColumnList);
-	getTable($historyNotesTable, data.notes, notesColumnList);
-	getTable($historyRatingsTable, data.ratings, ratingsColumnList);
+	getTable($historyObjectivesTable, data.objectives, objectivesColumnList, objectivesColumnDefs);
+	getTable($historyFeedbackTable, data.feedback, feedbackColumnList, feedbackColumnDefs);
+	getTable($historyDevelopmentNeedsTable, data.developmentNeeds, developmentNeedsColumnList, developmentNeedsColumnDefs);
+	getTable($historyNotesTable, data.notes, notesColumnList, notesColumnDefs);
+	getTable($historyRatingsTable, data.ratings, ratingsColumnList, ratingsColumnDefs);
 }
 
-function getTable(selectorId, dataset, columnsList){
-	if ($.fn.dataTable.isDataTable(selectorId)) {
-	    table = $(selectorId).dataTable();
-	    table.fnClearTable();
-	    table.fnAddData(dataset);
-	}else{
+function getTable(selectorId, dataset, columnsList, columnDefs){
 	 table = $(selectorId).dataTable({
+		columnDefs: columnDefs,
         data: dataset,
         columns: columnsList
 	 });
-	}
 }
