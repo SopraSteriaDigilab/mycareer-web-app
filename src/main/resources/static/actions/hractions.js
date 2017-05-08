8c5618d62d5d87000cafdc74978703339f1d3a64
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2017-05-08 14:42:18 +0100

    Removed unnecessary files. Started redoing hrdashboard

--- src/main/resources/static/actions/hractions.js
+++ src/main/resources/static/actions/hractions.js
@@ -1,5 +1,6 @@
-const HR = "/hr/";
-const GETCAREER = "getCareer/";
+const HR = "/hr";
+const GET_CAREER = "/getCareer";
+const GET_MY_CAREER_STATS = "/getMyCareerStats"
 
 /**
  *Ajax GET call get history for a user.
@@ -9,7 +10,20 @@ const GETCAREER = "getCareer/";
  * @param errorFunction function if call fails
  */
 function getEmployeeCareerAction(userId, searchUserId, successFunction, errorFunction){
-	var url = HR + userId  + "/" + GETCAREER + searchUserId;
+	var url = HR + "/" +userId + GET_CAREER + "/" + searchUserId;
+	var request = $get(url);
+	request.done( function(data){ 
+		successFunction(data)
+	});
+	request.fail(function(jqXHR, textStatus){
+        toastr.error(jqXHR.responseJSON.error);
+        errorFunction(jqXHR.responseJSON.error);
+	});
+}
+
+
+function getMyCareerStatsAction(successFunction, errorFunction){
+	var url = HR + GET_MY_CAREER_STATS;
 	var request = $get(url);
 	request.done( function(data){ 
 		successFunction(data)
